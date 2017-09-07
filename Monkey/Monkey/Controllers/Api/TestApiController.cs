ac9942e2028f31469b83f28f024f5047950194db
Top <topnguyen92@gmail.com>
2017-09-07 20:55:38 +0700

    update puppy

--- Monkey/Monkey/Controllers/Api/TestApiController.cs
+++ Monkey/Monkey/Controllers/Api/TestApiController.cs
@@ -1,8 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Monkey.Authentication;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System;
-using System.Net;
 
 namespace Monkey.Controllers.Api
 {
@@ -10,25 +6,13 @@ namespace Monkey.Controllers.Api
     public class TestApiController : ApiController
     {
         /// <summary>
-        ///     Test Exception 
+        ///     Test 
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        [Route("exception")]
-        [SwaggerResponse((int)HttpStatusCode.NoContent)]
-        public IActionResult TestException()
+        [Route("")]
+        public IActionResult Test()
         {
-            var token = TokenHelper.GenerateAccessToken(new
-            {
-                userId = 123456,
-                userName = "tonguyen"
-            }, TimeSpan.FromMinutes(30), null);
-
-            var a = TokenHelper.GetTokenData<object>(token.AccessToken);
-
-            bool isAuthenticated = System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
-
-            int nAn = int.Parse("Not a Number");
             return NoContent();
         }
     }
