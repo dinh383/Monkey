fa7d1195caa388ce94b6d1bcb4f5baf4ffd02914
Top <topnguyen92@gmail.com>
2017-09-13 00:32:57 +0700

    remove developers area and update puppy

--- Monkey/Monkey/Areas/Developers/Controllers/Base/DevelopersMvcController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Monkey.Areas.Developers.Controllers.Base
-{
-    [Route("developers")]
-    public class DevelopersMvcController : Controller
-    {
-    }
-}
