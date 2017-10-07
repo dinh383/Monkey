d83cf4a6434e3fb48065b65ec3cc42264e48a40c
Top <topnguyen92@gmail.com>
2017-10-07 14:57:29 +0700

    add bootbox

---
+++ Monkey/Monkey/Areas/Portal/wwwroot/js/Owl.js
@@ -0,0 +1,36 @@
+﻿
+var owl = {
+    initToolTip: function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    },
+
+    initBootBox: function () {
+        $('[data-plugin="confirm"]').click(function () {
+            var $this = $(this);
+            bootbox.dialog({
+                title: $this.data("confirm-title"),
+                message: $this.data("confirm-message"),
+                buttons: {
+                    danger: {
+                        label: "Confirm",
+                        className: "btn-danger",
+                        callback: function () {
+                            var action = $this.data("confirm-action");
+                            eval(action);
+                        }
+                    },
+                    main: {
+                        label: "Cancel",
+                        className: "btn-primary",
+                        callback: function () { }
+                    }
+                }
+            });
+        });
+    }
+};
+
+$(function() {
+    owl.initToolTip();
+    owl.initBootBox();
+});
