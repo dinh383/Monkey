b33e965eb358100b19c2d4a6abecd855cadf4ddf
Top <topnguyen92@gmail.com>
2017-10-08 20:57:31 +0700

    add mapper

---
+++ Monkey/Monkey.Mapper/Auth/RoleProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Monkey.Core.Entities.User;
+using Monkey.Core.Models.Auth;
+using Puppy.AutoMapper;
+
+namespace Monkey.Mapper.Auth
+{
+    public class RoleProfile : Profile
+    {
+        public RoleProfile()
+        {
+            CreateMap<RoleEntity, RoleModel>().IgnoreAllNonExisting();
+        }
+    }
+}
