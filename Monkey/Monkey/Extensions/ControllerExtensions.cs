50fa3c1ad1204429311e033a5ae9f0435bd1db03
Top Nguyen <topnguyen92@gmail.com>
2017-12-12 15:02:14 +0700

    update puppy and add more extension for controller

--- Monkey/Monkey/Extensions/ControllerExtensions.cs
+++ Monkey/Monkey/Extensions/ControllerExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnumsNET;
+using Microsoft.AspNetCore.Mvc;
+using Monkey.Core.Exceptions;
 using Puppy.Web;
 using System.Threading;
 
@@ -15,6 +17,13 @@ namespace Monkey.Extensions
             NotifyStatus status = NotifyStatus.Info)
         {
             controller.TempData.Set(Constants.TempDataKey.Notify, new NotifyResultViewModel(title, message, status));
+        }
+
+        public static void SetNotify(this Controller controller, string title, MonkeyException monkeyException, NotifyStatus status = NotifyStatus.Error)
+        {
+            var message = string.IsNullOrWhiteSpace(monkeyException.Message) ? monkeyException.Code.AsString(EnumFormat.Description) : monkeyException.Message;
+
+            controller.SetNotify(title, message, status);
         }
 
         public static void RemoveNotify(this Controller controller)
