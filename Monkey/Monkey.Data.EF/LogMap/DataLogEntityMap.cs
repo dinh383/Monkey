54498389e7fdfab56f06d516e5b6fbabe3181802
Top Nguyen <topnguyen92@gmail.com>
2018-01-29 10:10:48 +0700

    add log

---
+++ Monkey/Monkey.Data.EF/LogMap/DataLogEntityMap.cs
@@ -0,0 +1,54 @@
+﻿#region	License
+//------------------------------------------------------------------------------------------------
+// <License>
+//     <Copyright> 2018 © Top Nguyen → AspNetCore → Monkey </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Monkey → Entity Map </Project>
+//     <File>
+//         <Name> DataLogEntityMap.cs </Name>
+//         <Created> 28/01/2018 10:57:52 PM </Created>
+//         <Key> 6773530b-2810-4468-b28e-602a98ee65aa </Key>
+//     </File>
+//     <Summary>
+//         DataLogEntityMap.cs
+//     </Summary>
+// <License>
+//------------------------------------------------------------------------------------------------
+#endregion License
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Monkey.Core.Entities.DataLog;
+using Puppy.EF.Maps;
+
+namespace Monkey.Data.EF.LogMap
+{
+    public class DataLogEntityMap : TypeConfiguration<DataLogEntity>
+    {
+        public override void Map(EntityTypeBuilder<DataLogEntity> builder)
+        {
+            base.Map(builder);
+
+            builder.ToTable(nameof(DataLogEntity));
+
+            builder.HasKey(e => e.LogId);
+
+            builder.HasIndex(e => e.LogId);
+            builder.HasIndex(e => e.LogGlobalId);
+            builder.HasIndex(e => e.LogType);
+
+            builder.HasIndex(e => e.DataId);
+            builder.HasIndex(e => e.DataGlobalId);
+
+            builder.HasIndex(e => e.DataCreatedTime);
+            builder.HasIndex(e => e.DataCreatedBy);
+
+            builder.HasIndex(e => e.DataLastUpdatedTime);
+            builder.HasIndex(e => e.DataLastUpdatedBy);
+
+            builder.HasIndex(e => e.DataDeletedTime);
+            builder.HasIndex(e => e.DataDeletedBy);
+        }
+    }
+}
---
+++ Monkey/Monkey.Data.EF/LogMigrations/20180128162100_Initial.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Monkey.Data.EF.LogMigrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataLog",
+                columns: table => new
+                {
+                    LogId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DataCreatedBy = table.Column<int>(nullable: true),
+                    DataCreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    DataDeletedBy = table.Column<int>(nullable: true),
+                    DataDeletedTime = table.Column<DateTimeOffset>(nullable: true),
+                    DataGlobalId = table.Column<string>(nullable: true),
+                    DataGroup = table.Column<string>(nullable: true),
+                    DataId = table.Column<int>(nullable: false),
+                    DataJson = table.Column<string>(nullable: true),
+                    DataLastUpdatedBy = table.Column<int>(nullable: true),
+                    DataLastUpdatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    LogCreatedBy = table.Column<int>(nullable: true),
+                    LogCreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    LogGlobalId = table.Column<string>(nullable: true),
+                    LogHttpContextInfoJson = table.Column<string>(nullable: true),
+                    LogType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataLog", x => x.LogId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_DataCreatedBy",
+                table: "DataLog",
+                column: "DataCreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_DataCreatedTime",
+                table: "DataLog",
+                column: "DataCreatedTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_DataDeletedBy",
+                table: "DataLog",
+                column: "DataDeletedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_DataDeletedTime",
+                table: "DataLog",
+                column: "DataDeletedTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_DataGlobalId",
+                table: "DataLog",
+                column: "DataGlobalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_DataId",
+                table: "DataLog",
+                column: "DataId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_DataLastUpdatedBy",
+                table: "DataLog",
+                column: "DataLastUpdatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_DataLastUpdatedTime",
+                table: "DataLog",
+                column: "DataLastUpdatedTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_LogGlobalId",
+                table: "DataLog",
+                column: "LogGlobalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_LogId",
+                table: "DataLog",
+                column: "LogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataLog_LogType",
+                table: "DataLog",
+                column: "LogType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataLog");
+        }
+    }
+}
